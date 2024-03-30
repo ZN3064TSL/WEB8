0e69fe41179e2a771f366e31f4f405bb1723d959
ZN3064TSL <arikp8357@gmail.com>
2024-03-31 03:36:03 +0700

    Тренировки в полёте

--- ZN3064.py
+++ ZN3064.py
@@ -11,25 +11,5 @@ def workpiece():
 def training(prof):
     return render_template('training.html', prof=prof)
 
-@app.route('/list_prof/<list>')
-def list_prof(list):
-    profs = ['Инженер-исследователь',
-             'Пилот',
-             'Строитель',
-             'Экзобиолог',
-             'Врач',
-             'Инженер по терраформированию',
-             'Климатолог',
-             'Специалист по радиационной защите',
-             'Астрогеолог',
-             'Гляциолог',
-             'Инженер жизнеобеспечения',
-             'Метеоролог',
-             'Оператор марсохода',
-             'Киберинженер',
-             'Штурман',
-             'Пилот дронов']
-    return render_template('list_prof.html', list=list, profs=profs)
-
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
